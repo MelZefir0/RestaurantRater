87cf7a8aa3d5d5d5c7b6a941a78b678d13a7a6de
MelZefir0 <wanderlust3x3@gmail.com>
2017-11-08 20:45:43 -0500

    initial commit

---
+++ RestaurantRater/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+/// <summary>
+/// where the route is planned out
+/// </summary>
+namespace RestaurantRater
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+
+                //path to id page. Controller => action => id.
+                url: "{controller}/{action}/{id}",
+
+                                           //Controller
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
